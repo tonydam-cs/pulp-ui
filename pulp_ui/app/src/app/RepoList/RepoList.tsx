73dd3e95ef69562d19adf8b7df1b112e52ba45a4
SenecaQAnderson <senecaqanderson@gmail.com>
2022-02-25 13:03:36 -0500

    commiting in order to pull from master branch

--- pulp_ui/app/src/app/RepoList/RepoList.tsx
+++ pulp_ui/app/src/app/RepoList/RepoList.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+import * as PulpCoreClient from '@app/pulpcore-client';
 
 const RepoList= () => {
-    return <div>This is the repo list page!</div>
+    
+    const getRepoList = async () => {
+        const configuration = new PulpCoreClient.Configuration({username: 'admin', password: 'password', basePath: 'http://localhost:9000'});
+        const repoAPI = new PulpCoreClient.RepositoriesApi({configuration: configuration});
+        const tempList = await repoAPI.list();
+        console.log(list)
+        return tempList;
+    }
+    const [list, setList] = useState(getRepoList());
+
+    const getFormattedList = () => {
+        //let formattedList = [];
+        console.log(list)
+        return <p>This is placeholder text for the repo list</p>
+    }
+    return <div>
+        <h1>This is the repo list page!</h1>
+        {getFormattedList()}
+    </div>
 }
 
 export { RepoList };
