c63469a444994e5c1b1449222f1dfca9b441d0a0
SenecaQAnderson <senecaqanderson@gmail.com>
2022-03-16 19:55:48 -0400

    Added CSS file for RepoList; spaced buttons in RepoItem card

--- pulp_ui/app/src/app/RepoList/RepoItem.tsx
+++ pulp_ui/app/src/app/RepoList/RepoItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Card, CardHeader, CardBody } from '@patternfly/react-core';
+import './RepoList.css';
 /*
 List of possible props / fields returned by API call (full descriptions 
 at https://docs.pulpproject.org/pulpcore/restapi.html#operation/repositories_list):
@@ -19,10 +20,12 @@ const RepoItem = (props) => {
     return <div>
         <Card isHoverable>
             <CardHeader>{props.name && <h1>{props.name}</h1>}</CardHeader>
-            <CardBody>
+            <CardBody className='space-between-container'>
                 Last Sync: 
-                <Button>View Sync History</Button>
-                <Button>Sync</Button>
+                <div className='space-between-container'>
+                    <Button className='side-spacing' variant="secondary">View Sync History</Button>
+                    <Button className='side-spacing' variant="primary">Sync</Button>
+                </div>
             </CardBody>
         </Card>
     </div>
