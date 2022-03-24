bc0192412d1730222414224857bb30e1469fbf5f
SenecaQAnderson <senecaqanderson@gmail.com>
2022-03-24 19:44:03 -0400

    Modified RepoList card

--- pulp_ui/app/src/app/RepoList/RepoItem.tsx
+++ pulp_ui/app/src/app/RepoList/RepoItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, CardHeader, CardBody } from '@patternfly/react-core';
+import { Button, Card, CardHeader, CardBody, Divider } from '@patternfly/react-core';
 import './RepoList.css';
 /*
 List of possible props / fields returned by API call (full descriptions 
@@ -25,12 +25,21 @@ const RepoItem = (props) => {
                         <h1 className='card-header'>{props.name}</h1>
                     </Button>
                 }
+                {!props.name &&
+                    <Button variant='link'>
+                        <h1 className='card-header'>Repository Name Unavailable</h1>
+                    </Button>
+                }
             </CardHeader>
-            <CardBody className='space-between-container'>
-                <p className='side-spacing-left'>Last Sync: </p>
+            
+            <CardBody>
+            <Divider className='spacing-bottom' component='div'/>
                 <div className='space-between-container'>
-                    <Button className='side-spacing-right' variant="secondary">View Sync History</Button>
-                    <Button className='side-spacing-right' variant="danger">Sync</Button>
+                    <p className='spacing-left'>Last Sync: </p>
+                    <div className='space-between-container'>
+                        <Button className='spacing-right' variant="secondary">View Sync History</Button>
+                        <Button className='spacing-right' variant="danger">Sync</Button>
+                    </div>
                 </div>
             </CardBody>
         </Card>
